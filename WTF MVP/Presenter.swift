2c79f03d6417ddf328337bd42b287fe39d931bfa
albertojauregui <bastertwo@gmail.com>
2017-03-01 23:44:03 -0700

    Added presenter

---
+++ WTF MVP/Presenter.swift
@@ -0,0 +1,19 @@
+//
+//  Presenter.swift
+//  WTF MVP
+//
+//  Created by Alberto Jauregui on 01/03/17.
+//  Copyright © 2017 Alberto Jauregui. All rights reserved.
+//
+
+import Foundation
+
+protocol Presenter {
+    func viewDidLoad()
+    func viewWillAppear()
+}
+
+extension Presenter {
+    func viewDidLoad() { }
+    func viewWillAppear() { }
+}
