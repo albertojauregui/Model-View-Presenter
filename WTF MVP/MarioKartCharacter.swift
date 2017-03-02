cdc47aa48167d0f086c0c4d2daeb595117fe998f
albertojauregui <bastertwo@gmail.com>
2017-03-01 23:27:27 -0700

    Data model for mario kart character

---
+++ WTF MVP/MarioKartCharacter.swift
@@ -0,0 +1,14 @@
+//
+//  MarioKartCharacter.swift
+//  WTF MVP
+//
+//  Created by Alberto Jauregui on 01/03/17.
+//  Copyright © 2017 Alberto Jauregui. All rights reserved.
+//
+
+import Foundation
+
+struct MarioKartCharacter {
+    let name: String
+    let mail: String
+}
