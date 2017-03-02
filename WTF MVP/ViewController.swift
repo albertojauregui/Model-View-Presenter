1f033cac346c9e8ec1164370faec4c112ca40b12
albertojauregui <bastertwo@gmail.com>
2017-03-01 23:23:52 -0700

    Initial Commit

---
+++ WTF MVP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WTF MVP
+//
+//  Created by Alberto Jauregui on 01/03/17.
+//  Copyright © 2017 Alberto Jauregui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
