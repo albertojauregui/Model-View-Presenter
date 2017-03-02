843c25b22c9ec5160b179a0a07c1eb3850735eec
albertojauregui <bastertwo@gmail.com>
2017-03-01 23:33:43 -0700

    Added interaction for mario kart characters

---
+++ WTF MVP/MarioKartInteractor.swift
@@ -0,0 +1,32 @@
+//
+//  MarioKartInteractor.swift
+//  WTF MVP
+//
+//  Created by Alberto Jauregui on 01/03/17.
+//  Copyright © 2017 Alberto Jauregui. All rights reserved.
+//
+
+import Foundation
+
+class MarioKartInteractor {
+    fileprivate let marioKartCharacters: [MarioKartCharacter]
+    
+    init() {
+        self.marioKartCharacters = [
+            MarioKartCharacter(name: "Mario", mail: "mario@mariokart.com"),
+            MarioKartCharacter(name: "Luigi", mail: "luigi@mariokart.com")
+        ]
+    }
+    
+    func getMarioKartCharacters(_ callback:@escaping ([MarioKartCharacter]) -> Void){
+        delay(1.5) { 
+            callback(self.marioKartCharacters)
+        }
+    }
+    
+    fileprivate func delay(_ delay: Double, closure: @escaping ()->()){
+        DispatchQueue.main.asyncAfter(
+            deadline: DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: closure
+        )
+    }
+}
